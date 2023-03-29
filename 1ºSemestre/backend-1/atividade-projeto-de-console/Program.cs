cc0d411d1180a169c88555563e3183864a83d900
Filipe De Oliveira Góis <46633783825@info.local>
2023-03-29 16:30:57 -0300

    novo

---
+++ 1ºSemestre/backend-1/atividade-projeto-de-console/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@$"
+
+                                         --------------------------
+                                         |     Programa para       |
+                                         |     Calcular idade      |
+                                         --------------------------
+");
+Console.ResetColor();
+
+Console.WriteLine($"Insira sua idade em anos: ");
+int idadeAnos = int.Parse(Console.ReadLine());
+
+
+
+int idadeMeses = idadeAnos*12;
+int idadeDias = idadeMeses*30;
+int idadeHoras = idadeDias*24;
+int idadeMinutos = idadeDias*60;
+
+
+Console.WriteLine($"{idadeMeses} meses {idadeDias} dias {idadeHoras} horas {idadeMinutos} minutos");

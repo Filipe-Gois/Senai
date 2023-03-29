35682f7f03dac22a65afc30d51fbeee2cff4e02c
Filipe De Oliveira Góis <46633783825@info.local>
2023-03-29 17:24:26 -0300

    novo

---
+++ 1ºSemestre/backend-1/atividade-projeto-de-console/atividade2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@$"
+
+                                         --------------------------
+                                         |     Programa para       |
+                                         |     Calcular idade      |
+                                         --------------------------
+");
+Console.ResetColor();
+
+var dataEntrada = DateTime.Now;
+
+Console.WriteLine(dataEntrada);
+
+Console.WriteLine($"Digite o ano em que você nasceu:");
+int ano = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o mês em que você nasceu:");
+int mes = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o dia em que você nasceu:");
+int dia = int.Parse(Console.ReadLine());
+
+int idadeAnos = dataEntrada.Year-ano;
+int idadeSem = idadeAnos*12*4;
+
+Console.WriteLine($"{idadeAnos} é sua idade em anos, tendo {idadeSem} semanas de vida.");
+
+
+
+
+
+
---
+++ 1ºSemestre/backend-1/projeto-IMC/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Crie um programa para calcular o imc de uma pessoa
+// Entrada: nome, idade, peso, altura.
+// processamento> peso dividido po altura ao quadrado
+
+// Entrada
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@$"
+
+-----------------------
+|   Programa para     |
+|   Calcular IMC      |
+-----------------------
+");
+Console.ResetColor();
+
+Console.WriteLine($"Digite o nome do paciente: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Bem vindo ao nosso sistema {nome}");
+
+Console.WriteLine($"Informe o peso do paciente ");
+float peso = float.Parse(Console.ReadLine());
+
+// só exemplo
+
+Console.WriteLine($"Informe a altura do paciente: ");
+float altura = float.Parse(Console.ReadLine());
+
+float imc = peso / ((float)Math.Pow(altura,2));
+
+Console.WriteLine($"O paciente {nome} tem IMC igual a: {imc}");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Obrigado por utilizar nosso sistema !");
+Console.ResetColor();
+
+
+
+
+
+
+
+
+
+

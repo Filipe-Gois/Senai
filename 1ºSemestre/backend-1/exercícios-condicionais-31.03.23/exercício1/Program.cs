fda9bcafaf26382389651efab9bde25edae9d727
Filipe De Oliveira Góis <46633783825@info.local>
2023-03-31 17:14:35 -0300

    Novo

---
+++ 1ºSemestre/backend-1/exercícios-condicionais-31.03.23/exercício1/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine($"Em que ano você nasceu ?");
+int ano = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Em qual mês você nasceu ?");
+int mes = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Em que dia você nasceu ?");
+int dia = int.Parse(Console.ReadLine()!);
+
+
+
+
+if ((ano > 2013) || (mes == 0) || (mes > 12))
+{
+    Console.WriteLine($"Por favor, redija seu ano e mês de nascimento.");
+}
+
+else if ((ano > 2013) || (dia == 0) || (dia > 31) )
+{
+    Console.WriteLine($"Por favor, redija seu ano e dia de nascimento.");
+    
+}
+
+else if ((mes == 0) || (mes > 12) || (dia == 0) || (dia > 31))
+{
+    Console.WriteLine($"Por favor, redija seu mês e dia de nascimento.");
+    
+}
+
+else if ((ano > 2013) || (mes == 0) || (mes > 12) || (dia == 0) || (dia > 31))
+{
+Console.WriteLine($"Por favor, redija suas informações.");
+
+}
+
+else if (ano > 2013 )
+{
+    Console.WriteLine($"Data inválida. Preecha com um ano válido até 2013.");
+}
+
+else if ((mes == 0) && (mes > 12) )
+{
+    Console.WriteLine($"Data inválida. Preencha com um mês válido até 12.");
+}
+
+else if ((dia == 0) && (dia > 31))
+{
+    Console.WriteLine($"Data inválida. Preencha com um dia válido até 31.");
+    
+}
+
+else
+{
+    Console.WriteLine($"Tudo nos conformes. Prossiga com o seu login.");
+}
---
+++ 1ºSemestre/backend-1/exercícios-condicionais-31.03.23/exercício3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine($"Qual o raio da circunferência ? ");
+float raio = float.Parse(Console.ReadLine()!);
+
+float area = (Math.PI) * (Math.Pow(raio, 2));
+
+Console.WriteLine($"{area}");

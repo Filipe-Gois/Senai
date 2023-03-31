a51f4b6d07ec85cd1a61421e32ea7a12da9f40fa
Filipe Góis <fythoy@gmail.com>
2023-03-30 21:20:12 -0300

    novo

---
+++ 1ºSemestre/backend-1/exercicios-condicionais/exercício6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine($"Olá, informe sua média bimestral:");
+float media = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Informe sua frequência de acompanhamento das aulas:");
+float freq = float.Parse(Console.ReadLine()!);
+
+if ((media >= 7) && (freq >= 75))
+{
+    Console.WriteLine($"Parabéns, você foi aprovado.");
+    
+}
+
+else if ((media < 7) || (freq < 75))
+{
+    Console.WriteLine($"Sinto muito, você foi reprovado.");
+    
+}

60fc194b0ecd824a1ec8053fd57ff02ac58a14e5
Filipe De Oliveira Góis <46633783825@info.local>
2023-04-03 17:21:32 -0300

    Novo

---
+++ 1ºSemestre/backend-1/switch/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine($"Inofrme o número correspondente ao dia da semana:");
+int diaSemana = int.Parse(Console.ReadLine()!);
+
+switch (diaSemana)
+{
+    case 1:
+        Console.WriteLine($"Hoje é domingo.");
+        break;
+
+    case 2:
+        Console.WriteLine($"Hoje é segunda.");
+        break;
+
+    case 3:
+        Console.WriteLine($"Hoje é terça.");
+        break;
+
+    case 4:
+        Console.WriteLine($"Hoje é quarta.");
+        break;
+
+    case 5:
+        Console.WriteLine($"Hoje é quinta.");
+        break;
+
+    case 6:
+        Console.WriteLine($"Hoje é sexta.");
+        break;
+
+    case 7:
+        Console.WriteLine($"Hoje é sábado.");
+        break;
+
+    default:
+        Console.WriteLine($"O dia informado não corresponde a nenhum dia da semana.");
+        break;
+}
---
+++ 1ºSemestre/backend-1/switch/escolha de refri exercício/Program.cs
@@ -0,0 +1,79 @@
+﻿Console.WriteLine(@$"
+-------------------------------------
+| Bem-vindo!                          |   
+|                                     | 
+|   Informe a bebida desejada:        |
+|                                     |
+|    1) Coca Cola                     |
+|    2) Pepsi                         |
+|    3) Fanta                         |
+|    4) Monster                       |
+--------------------------------------
+");
+
+Console.WriteLine($"Informe a bebida desejada:");
+
+int bebida = int.Parse(Console.ReadLine()!.ToUpper());
+string respGelo = "";
+
+switch (bebida)
+{
+    case 1:
+    Console.WriteLine($"Gostaria de acrescentar gelo ?");
+    respGelo = Console.ReadLine()! .ToUpper();
+    
+    
+    if (respGelo == "SIM") {
+        Console.WriteLine($"Coca Cola com gelo adicionada ao pedido.");
+    }
+    else{
+        Console.WriteLine($"Coca Cola sem gelo adicionada ao pedido.");
+        
+    }
+    break;
+
+    case 2:
+    Console.WriteLine($"Gostaria de acrescentar gelo ?");
+    respGelo = Console.ReadLine()! .ToUpper();
+    
+    
+    if (respGelo == "SIM") {
+        Console.WriteLine($"Pepsi com gelo adicionada ao pedido.");
+    }
+    else{
+        Console.WriteLine($"CPepsi sem gelo adicionada ao pedido.");
+        
+    }
+    break;
+
+    case 3:
+    Console.WriteLine($"Gostaria de acrescentar gelo ?");
+    respGelo = Console.ReadLine()! .ToUpper();
+    
+    
+    if (respGelo == "SIM") {
+        Console.WriteLine($"Fanta com gelo adicionada ao pedido.");
+    }
+    else{
+        Console.WriteLine($"Fanta sem gelo adicionada ao pedido.");
+        
+    }
+    break;
+    
+    case 4:
+    Console.WriteLine($"Gostaria de acrescentar gelo ?");
+    respGelo = Console.ReadLine()! .ToUpper();
+    
+    
+    if (respGelo == "SIM") {
+        Console.WriteLine($"Monter com gelo adicionada ao pedido.");
+    }
+    else{
+        Console.WriteLine($"Monster sem gelo adicionada ao pedido.");
+        
+    }
+    break;
+    
+    default:
+    break;
+}

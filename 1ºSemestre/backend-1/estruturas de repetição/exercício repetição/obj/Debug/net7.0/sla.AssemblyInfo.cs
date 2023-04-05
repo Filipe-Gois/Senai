5a010b5ead198786fa46f72ca45243c422e5a588
Filipe De Oliveira Góis <46633783825@info.local>
2023-04-05 15:40:50 -0300

    Novo

--- 1ºSemestre/backend-1/estruturas de repetição/exercício repetição/obj/Debug/net7.0/sla.AssemblyInfo.cs
+++ 1ºSemestre/backend-1/estruturas de repetição/exercício repetição/obj/Debug/net7.0/sla.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("exercício")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("sla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("exercício")]
-[assembly: System.Reflection.AssemblyTitleAttribute("exercício")]
+[assembly: System.Reflection.AssemblyProductAttribute("sla")]
+[assembly: System.Reflection.AssemblyTitleAttribute("sla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.

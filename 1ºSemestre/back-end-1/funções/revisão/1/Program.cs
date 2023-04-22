c70853fd816b7f692c7a39b412cb789324586ffa
Filipe Góis <fythoy@gmail.com>
2023-04-21 21:49:53 -0300

    Novo

---
+++ 1ºSemestre/back-end-1/funções/revisão/1/Program.cs
@@ -0,0 +1,39 @@
+﻿// //  não obrigatórios:(local  tipo). retorno nome    parâmetros
+//     public static void Executar(string tete)
+// {
+
+// }
+
+// static int Calcular()
+// {
+//     int a = 1;
+//     int b = 7;
+//     int c = a + b;
+    
+//     return c;
+    
+// }
+
+
+// Console.WriteLine(Calcular());
+// Console.WriteLine(Calcular());
+// Console.WriteLine(Calcular());
+
+
+// função para exobor tabuada
+
+static void Tabuada(int numero)
+{   
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($"{numero} X {i} = {numero * i}");
+    }
+    Console.WriteLine();
+}
+
+
+Tabuada(144);
+Tabuada(14);
+Tabuada(3);
+Tabuada(7);
+

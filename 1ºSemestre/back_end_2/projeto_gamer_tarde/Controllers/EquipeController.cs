463bcf55f3f0b1b366d0ae47ab415fd2940e7239
Filipe De Oliveira Góis <46633783825@info.local>
2023-06-01 17:22:18 -0300

    Novo

--- 1ºSemestre/back_end_2/projeto_gamer_tarde/Controllers/EquipeController.cs
+++ 1ºSemestre/back_end_2/projeto_gamer_tarde/Controllers/EquipeController.cs
@@ -54,7 +54,7 @@ namespace projeto_gamer_tarde.Controllers
 
                 var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
 
-                if (Directory.Exists(folder))
+                if (!Directory.Exists(folder))
                 {
                     Directory.CreateDirectory(folder);
                 }
@@ -88,6 +88,69 @@ namespace projeto_gamer_tarde.Controllers
         public IActionResult Error()
         {
             return View("Error!");
+        }
+
+        [Route("Excluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+           Equipe x = c.Equipe.First(x => x.IdEquipe == id);
+
+           c.Equipe.Remove(x);
+
+           c.SaveChanges();
+
+           return LocalRedirect("~/Equipe/Listar");
+        }
+
+        [Route("Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+            Equipe x = c.Equipe.First(x => x.IdEquipe == id);
+
+            ViewBag.Equipe = x;
+
+            return View("Edit");
+        }
+
+        [Route("Atualizar")]
+        public IActionResult Atualizar(IFormCollection form, Equipe e)
+        {
+            Equipe novaEquipe = new Equipe();
+            novaEquipe.Nome = e.Nome;
+
+            // upload da imagem na equipe nova(atualizada)
+            if (form.Files.Count > 0)
+            {
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                var path = Path.Combine(folder, file.FileName);
+                
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                novaEquipe.Imagem = file.FileName;
+            }
+            else{
+                novaEquipe.Imagem = "padrao.png";
+            }
+
+            Equipe equipe = c.Equipe.First(x => x.IdEquipe == e.IdEquipe);
+
+            equipe.Nome = novaEquipe.Nome;
+            equipe.Imagem = novaEquipe.Imagem;
+
+            c.Equipe.Update(equipe);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Equipe/Listar");
         }
     }
 }
---
+++ 1ºSemestre/back_end_2/projeto_gamer_tarde/wwwroot/img/Equipes/SQLQuery1_30.05.2023.sql
@@ -0,0 +1,7 @@
+select * from Equipe
+
+insert into Equipe
+values('Ibis', 'ibis.png')
+
+insert into Equipe
+values('Barcelona', 'barcelona.png')

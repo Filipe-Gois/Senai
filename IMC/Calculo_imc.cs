235646302a46d7aa18d8652c46cb545f3d9d56d9
Filipe Góis <fythoy@gmail.com>
2023-07-21 17:11:56 -0300

    Novo

---
+++ IMC/Calculo_imc.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    public class Calculo_imc
+    {
+        public string? nome { get; set; }
+        public int idade { get; set; }
+        public double peso { get; set; }
+        public double altura { get; set; }
+        public double resultado { get; set; }
+
+
+        public string CalcularImc()
+        {
+            resultado = peso / Math.Pow(altura, 2);
+
+            if (resultado < 17)
+            {
+                return "Você está abaixo do peso.";
+            }
+
+            else if (resultado < 18.5)
+            {
+                return "Você está abaixo do peso.";
+
+            }
+
+            else if (resultado < 25)
+            {
+                return "Seu peso está normal.";
+
+            }
+
+            else if (resultado < 30)
+            {
+                return "Você está acima do peso.";
+
+            }
+
+            else if (resultado < 35)
+            {
+                return "Obesidade I.";
+
+            }
+
+            else if (resultado < 40)
+            {
+                return "Obesidade II.";
+
+            }
+
+            else
+            {
+                return "Obesidade III.";
+
+            }
+
+
+        }
+    }
+}
---
+++ IMC/Program.cs
@@ -0,0 +1,29 @@
+﻿using IMC;
+
+Calculo_imc imc = new Calculo_imc();
+
+
+Console.WriteLine($"Olá, informe seu nome:");
+imc.nome = Console.ReadLine()!;
+
+Console.WriteLine($"Informe sua idade:");
+imc.idade = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Informe seu peso:");
+imc.peso = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Informe sua altura:");
+imc.altura = double.Parse(Console.ReadLine()!);
+
+
+
+Console.WriteLine(@$"
+
+Nome do paciente: {imc.nome}
+Idade: {imc.idade}
+Peso: {imc.peso}
+Altura: {imc.altura}");
+
+
+
+Console.WriteLine(imc.CalcularImc());

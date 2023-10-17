541cb27192836f6aaa7f100a8937d704ced4695c
Filipe Góis <fythoy@gmail.com>
2023-10-17 17:19:50 -0300

    Novo

---
+++ 2ºSemestre/Sprint 3/JavaScript/Semana 1/objetos2.js
@@ -0,0 +1,14 @@
+let filipe = {} //objeto literal
+
+let gui = new Object(); //ngm mais usa esta bomba
+
+let murilo = {
+    nome: "Murilo",
+    idade: 17,
+    altura: 1.71
+}
+
+gui.nome = "Guilherme"
+gui.idade = 18
+
+console.log(gui)

bfc8e190c8ca6ebc6a73ac87ced3dec496ea1d2d
Filipe Góis <fythoy@gmail.com>
2023-10-26 21:39:43 -0300

    novo

--- 2ºSemestre/Sprint 3/ContactForm/script.js
+++ 2ºSemestre/Sprint 3/ContactForm/script.js
@@ -1,68 +1,103 @@
-const urlLocal = "http://localhost:3000/contatos"
+//1ºpasso: definir as variávei
+
+//url da api local
+let urlLocal = "http://localhost:3000/contatos"
 
 
-let nome = document.getElementById('nome').value
-let sobrenome = document.getElementById('sobrenome').value
-let email = document.getElementById('email').value
 
-let pais = document.getElementById('pais').value
-let ddd = document.getElementById('pais').value
-let telefone = document.getElementById('telefone').value
+//2º passo: criar a API local
 
-let cep = document.getElementById('cep').value
-let rua = document.getElementById('rua').value
-let numeroCasa = document.getElementById('numero').value
-let complemento = document.getElementById('complemento').value
-let bairro = document.getElementById('bairro').value
-let cidade = document.getElementById('cidade').value
-let uf = document.getElementById('UF').value
-
-let anotacoes = document.getElementsByClassName('anotacoes').value
-
-let dados = {
-    nome,
-    sobrenome,
-    email,
-    pais,
-    ddd,
-    telefone,
-    cep,
-    rua,
-    numeroCasa,
-    complemento,
-    bairro, cidade,
-    uf,
-    anotacoes
-}
-
+//3º passo: chamar a API "VIACEP" para fazer a validação do cep informado
 
 async function chamarApi() {
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-    try {
-        const promise = await fetch(url);
-        const endereco = await promise.json();
+    let cep = document.getElementById('cep').value
+    let urlCep = `https://viacep.com.br/ws/${cep}/json/`
 
-        alert('Cep encontrado!!')
+    try {
+        alert("Cep encontrado")
+        let promise = await fetch(urlCep);
+        let endereco = await promise.json();
         console.log(endereco);
-        exibirEndereco(endereco);
+
+        //exibe os dados buscados pelo cep dentro dos valores dos inputs
+        exibirDados(endereco);
     }
     catch (error) {
         alert('Cep inválido!!')
+        limparDados()
     }
 }
 
-function cadastrar(dados) {
+// 4º passo: Criar método para exibição de dados de endereço
+function exibirDados(e) {
 
+    document.getElementById('rua').value = e.logradouro;
+    document.getElementById('bairro').value = e.bairro;
+    document.getElementById('cidade').value = e.localidade
+    document.getElementById('UF').value = e.uf;
 }
-
-function exibirEndereco(e) {
-    rua = e.logradouro
-    bairro = e.bairro
-    cidade = e.cidade
-    uf = e.uf
-}
+// 5º passo:criar método para limpar os campos do formulário
 
 function limparDados() {
-
+    document.getElementById('rua').value = ""
+    document.getElementById('numero').value = ""
+    document.getElementById('complemento').value = ""
+    document.getElementById('bairro').value = ""
+    document.getElementById('cidade').value = ""
+    document.getElementById('UF').value = ""
 }
 
+// 6º passo:criar método para cadastrar um contato na lista
+
+async function cadastrar(e) {
+    e.preventDefault()
+    let nome = document.getElementById('nome').value
+    let sobrenome = document.getElementById('sobrenome').value
+    let email = document.getElementById('email').value
+
+    let pais = document.getElementById('pais').value
+    let ddd = document.getElementById('pais').value
+    let telefone = document.getElementById('telefone').value
+
+    let cep = document.getElementById('cep').value
+    let rua = document.getElementById('rua').value
+    let numeroCasa = document.getElementById('numero').value
+    let complemento = document.getElementById('complemento').value
+    let bairro = document.getElementById('bairro').value
+    let cidade = document.getElementById('cidade').value
+    let uf = document.getElementById('UF').value
+
+    let anotacoes = document.getElementsByClassName('anotacoes').value
+
+    let dados = {
+        nome,
+        sobrenome,
+        email,
+        pais,
+        ddd,
+        telefone,
+        cep,
+        rua,
+        numeroCasa,
+        complemento,
+        bairro, cidade,
+        uf,
+        anotacoes
+    }
+
+
+    try {
+        let promise = await fetch(urlLocal, {
+            body: JSON.stringify(dados),
+            headers: { "Content-Type": "application/JSON" },
+            method: "post",
+        });
+
+        let retorno = promise.json();
+        console.log(retorno);
+    }
+    catch (error) {
+        alert(error)
+    }
+
+}

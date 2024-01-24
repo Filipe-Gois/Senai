d591a569fe126db9bef7f338c71825a6046800ab
Filipe Góis <fythoy@gmail.com>
2024-01-24 17:22:38 -0300

    novo

---
+++ 3ºSemestre/Sprint 1/teste/helloWorld/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragrafo}>Hello, World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paragrafo: {
+    color: 'red',
+  }
+});

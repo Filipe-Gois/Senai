d31893a53450bfe1b8ed0236bca956a5e537dcfa
Filipe Góis <fythoy@gmail.com>
2024-03-11 17:22:10 -0300

    Added instructions for installing Expo CLI globally and using the `<ActivityIndicator />` component for loading. Removed the `sla.txt` file.

---
+++ 3ºSemestre/Sprint 2/gpsexemplo/gpsexemplo/App.js
@@ -0,0 +1,336 @@
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import {
+  requestForegroundPermissionsAsync, //solikcitar permissao da localização
+  getCurrentPositionAsync, //captura a localização atual
+} from "expo-location";
+import { useEffect, useState } from "react";
+
+import MapViewDirections from "react-native-maps-directions";
+import { mapsKey } from "./Utils/MapsKey";
+
+const App = () => {
+  const [initialPosition, setInitialPosition] = useState(null);
+
+  const CapturarLocalizacao = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+
+      setInitialPosition(currentPosition);
+
+      console.log(initialPosition);
+    }
+  };
+
+  useEffect(() => {
+    CapturarLocalizacao();
+  }, [1000]);
+
+  return (
+    <View style={styles.container}>
+      {initialPosition !== null ? (
+        <>
+          <MapView
+            initialRegion={{
+              latitude: initialPosition.coords.latitude,
+              longitude: initialPosition.coords.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}
+            style={styles.map}
+            customMapStyle={grayMapStyle}
+            provider={PROVIDER_GOOGLE}
+          >
+            {/* criando um marcador no mapa */}
+            <Marker
+              coordinate={{
+                latitude: initialPosition.coords.latitude,
+                longitude: initialPosition.coords.longitude,
+              }}
+              title="Exemplo de local"
+              description="Senai Paulo Antonio Skaf"
+              pinColor="blue"
+
+              // propriedade para alterar o ícone de localização
+              // image={}
+            />
+
+            <MapViewDirections
+              origin={initialPosition.coords}
+              destination={{
+                latitude: -23.5068153,
+                longitude: -46.5159653,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              }}
+              strokeWidth={5}
+              strokeColor="#496DBA"
+              apikey={mapsKey}
+            />
+
+            <Marker
+              coordinate={{
+                latitude: -23.5068153,
+                longitude: -46.5159653,
+              }}
+              title="Exemplo de local"
+              description="Senai Paulo Antonio Skaf"
+              pinColor="red"
+
+              // propriedade para alterar o ícone de localização
+              // image={}
+            />
+          </MapView>
+          {console.log(initialPosition)}
+        </>
+      ) : (
+        <>
+          <Text>Carregando...</Text>
+
+          <ActivityIndicator />
+        </>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    flex: 1,
+    width: "100%",
+  },
+});
+
+const grayMapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#E1E0E7",
+      },
+    ],
+  },
+  {
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        saturation: -5,
+      },
+      {
+        lightness: -5,
+      },
+    ],
+  },
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#33303E",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.business",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#66DA9F",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1B1B1B",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#C6C5CE",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ACABB7",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8EA5D9",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+];
+
+export default App;

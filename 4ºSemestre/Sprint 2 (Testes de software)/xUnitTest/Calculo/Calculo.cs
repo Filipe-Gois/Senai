7bea48d2f43fcaaf3dc5c6228eda9768bcd2f8d6
Filipe <fythoy@gmail.com>
2024-07-31 17:24:39 -0300

    .

---
+++ 4ºSemestre/Sprint 2 (Testes de software)/xUnitTest/Calculo/Calculo.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+
+namespace Calculos
+{
+    public static class Calculo
+    {
+        public static double Somar(double x, double y) => x + y;
+        public static double Subtrair(double x, double y) => x - y;
+        public static double Dividir(double x, double y) => x / y;
+        public static double Multiplicar(double x, double y) => x * y;
+        public static double Modulo(double x) => x * -1;
+        public static double Bhaskara(double a, double b, double c)
+        {
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+
+
+        };
+    }
+}
---
+++ 4ºSemestre/Sprint 2 (Testes de software)/xUnitTest/Calculos.Test/CalculoUnitTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos.Test
+{
+    public class CalculoUnitTest
+    {
+        [Fact]
+        public void SomarDoisNumeros()
+        {
+            double n1 = 10;
+            double n2 = 5;
+            double valorEsperadoSoma = 15;
+
+            double soma = Calculo.Somar(n1, n2);
+
+            Assert.Equal(valorEsperadoSoma, soma);
+
+        }
+        [Fact]
+        public void SubtrairDoisNumeros()
+        {
+            double n1 = 10;
+            double n2 = 5;
+            double valorEsperado = 5;
+
+
+            double subtracao = Calculo.Subtrair(n1, n2);
+
+            Assert.Equal(valorEsperado, subtracao);
+
+        }
+        [Fact]
+        public void DividirDoisNumeros()
+        {
+            double n1 = 10;
+            double n2 = 5;
+            double valorEsperado = 2;
+            double divisao = Calculo.Dividir(n1, n2);
+
+            Assert.Equal(valorEsperado, divisao);
+        }
+        [Fact]
+        public void MultiplicarDoisNumeros()
+        {
+            double n1 = 10;
+            double n2 = 5;
+            double valorEsperadoSoma = 50;
+
+            double multiplicacao = Calculo.Multiplicar(n1, n2);
+
+            Assert.Equal(valorEsperadoSoma, multiplicacao);
+        }
+        [Fact]
+        public void Modulo()
+        {
+            double n1 = -20;
+
+            double valorEsperaModulo = n1 * -1;
+
+            double modulo = Calculo.Modulo(n1);
+
+            Assert.Equal(valorEsperaModulo, modulo);
+        }
+    }
+}
